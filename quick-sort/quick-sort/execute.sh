acb92a7082e31af3cf5c2f4c1b58d412c69f3e63
Suppakorn Rakna <tap.pt.33@gmail.com>
2023-03-14 17:39:28 +0700

    `fix`: corrupted size error

--- quick-sort/quick-sort/execute.sh
+++ quick-sort/quick-sort/execute.sh
@@ -1,6 +1,6 @@
 for i in {1..12}; do
   echo "Number of processes: ${i}"
   for j in {1..7}; do
-    mpirun -n "${i}" -hostfile ../hosts.txt ./quick-sort-parallel ./random10.txt
+    mpirun -n "${i}" -hostfile ../hosts.txt ./quick-sort-parallel ./random10000000.txt
   done
 done
